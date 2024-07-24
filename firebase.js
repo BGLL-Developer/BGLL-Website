9e35980f7bd7a77329bb36a95eaa75973dc07a65
AlexG-BGLL-Laptop <support@lotteries.bz>
2024-07-23 20:28:26 -0600

    Added Monitor Display

--- firebase.js
+++ firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Import the necessary Firebase modules
-import { getFirestore, doc, getDocs, collection, query, where } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getFirestore, doc, getDocs, collection, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 // --------------------------------------------------------------------------------------------
 let countrer = 0;
@@ -30,7 +30,16 @@ function todayWinningNumber(dateString) {
 
         if (winningNumbers.length > 0) {
             const data = winningNumbers[0];
-            document.getElementById('date').innerText = new Date(dateString).toDateString();
+
+            const parsedDate = new Date(dateString)
+
+            if (isNaN(parsedDate.getDate())) {
+                document.getElementById('date').innerText = dateString;
+            }
+            else {
+                document.getElementById('date').innerText = parsedDate.toDateString();
+            }
+
             document.getElementById('winningNumber').innerText = data.winningNumber;
         } else {
             console.log("No data available for", dateString);
@@ -201,7 +210,123 @@ function populateAgentsTable(agents) {
     }
 }
 
-export { todayWinningNumber, searchWinningNumber, searchAgent };
+function getMondayOfCurrentWeek() {
+    const now = new Date();
+    const dayOfWeek = now.getDay(); // 0 is Sunday, 1 is Monday, ..., 6 is Saturday
+    const diff = now.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1); // Adjust for Monday being the start of the week
+    const monday = new Date(now.setDate(diff));
+    return monday;
+}
+
+function getFridayOfCurrentWeek() {
+    const monday = getMondayOfCurrentWeek();
+    const friday = new Date(monday);
+    friday.setDate(monday.getDate() + 4); // Friday is 4 days after Monday
+    return friday;
+}
+
+function listenForChangesBoledo() {
+    const db = getFirestore(app);
+    const boledoRef = collection(db, "Boledo");
+    const monday = getMondayOfCurrentWeek();
+    const friday = getFridayOfCurrentWeek();
+
+    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+    const dayElements = {
+        'Mon': 'boledoWinningNumberMon',
+        'Tue': 'boledoWinningNumberTue',
+        'Wed': 'boledoWinningNumberWed',
+        'Thu': 'boledoWinningNumberThu',
+        'Fri': 'boledoWinningNumberFri'
+    };
+
+    const stringMonday = monday.toLocaleDateString();
+    const stringFriday = friday.toLocaleDateString();
+
+    const q = query(boledoRef,
+        where("date", ">=", stringMonday),
+        where("date", "<=", stringFriday),
+        where('status', '==', 'active'));
+
+    getDocs(q).then((querySnapshot) => {
+        const winningNumbers = querySnapshot.docs.map((doc) => doc.data());
+
+        if (winningNumbers.length > 0) {
+            daysOfWeek.forEach(day => {
+                const dayElement = document.getElementById(dayElements[day]);
+                dayElement.innerHTML = ''; // Clear previous numbers
+            });
+
+            winningNumbers.forEach(data => {
+                const winningDate = new Date(data.date);
+                const dayOfWeek = winningDate.toLocaleDateString('en-US', { weekday: 'short' });
+
+                if (daysOfWeek.includes(dayOfWeek)) {
+                    const dayElement = document.getElementById(dayElements[dayOfWeek]);
+                    dayElement.innerText = `${data.winningNumber}`;
+                }
+            });
+        } else {
+            console.log("No data available for this week");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function getSundayOfCurrentWeek() {
+    const now = new Date();
+    const dayOfWeek = now.getDay(); // 0 is Sunday, 1 is Monday, ..., 6 is Saturday
+    const diff = now.getDate() - dayOfWeek; // Calculate days since the last Sunday
+    const sunday = new Date(now.setDate(diff));
+    return sunday;
+}
+
+function listenForChangesJackpot() {
+    const db = getFirestore(app);
+    const jackpotRef = collection(db, "Jackpot");
+    const sunday = getSundayOfCurrentWeek();
+
+    const stringSunday = sunday.toLocaleDateString();
+
+    console.log(stringSunday)
+
+    const q = query(jackpotRef,
+        where("date", "==", stringSunday),
+        where('status', '==', 'active'));
+
+    getDocs(q).then((querySnapshot) => {
+        const winningNumbers = querySnapshot.docs.map((doc) => doc.data());
+
+        if (winningNumbers.length > 0) {
+            const jackpotFirstNumbersElement = document.getElementById('jackpotFirstWinningNumber');
+            const jackpotSecondNumbersElement = document.getElementById('jackpotSecondWinningNumber');
+            const jackpotThirdNumbersElement = document.getElementById('jackpotThirdWinningNumber');
+            jackpotFirstNumbersElement.innerHTML = ''; // Clear previous numbers
+            jackpotSecondNumbersElement.innerHTML = ''; // Clear previous numbers
+            jackpotThirdNumbersElement.innerHTML = ''; // Clear previous numbers
+
+            winningNumbers.forEach(data => {
+                const lastTwoDigits = data.firstWinningNumber.slice(-2);
+                const restOfNumber = data.firstWinningNumber.slice(0, -2);
+
+                jackpotFirstNumbersElement.innerHTML = `${restOfNumber}<span class="underline">${lastTwoDigits}</span>`;
+
+                jackpotSecondNumbersElement.innerText = `${data.secondWinningNumber}`;
+
+                jackpotThirdNumbersElement.innerText = `${data.thirdWinningNumber}`;
+            });
+        } else {
+            console.log("No data available for this Sunday");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+
+
+export { todayWinningNumber, searchWinningNumber, searchAgent, listenForChangesBoledo, listenForChangesJackpot };
 
 
 
